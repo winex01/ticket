d972f9ec921649b6a29a6804e7cda8fd5cbd6e9a
winex01 <winnie131212592@gmail.com>
2019-07-06 19:04:59 +0800

    auto select site if it came from site create ticket

--- app/Http/Controllers/SiteTicketController.php
+++ app/Http/Controllers/SiteTicketController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 class SiteTicketController extends Controller
 {
     //
-    public function create($id)
+    public function create($id, Request $request)
     {
-    	clock($id);
+    	// clock($id);
+    	session(['site_id' => $id]);
+    	return redirect()->route('voyager.tickets.create');
     }
 }
